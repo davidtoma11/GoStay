a997e1687aff4082f45a174b906070aa61d2d422
davidtoma11 <tomadavid2004@yahoo.com>
2026-01-06 00:10:46 +0200

    bug fixed

--- scripts/utils/tracker.php
+++ scripts/utils/tracker.php
@@ -1,13 +1,17 @@
 <?php
+if (!isset($db)) {
+    require_once __DIR__ . '/../config/database.php';
+    $database = new Database();
+    $db = $database->getConnection();
+}
 
-if (isset($db)) {
-    try {
-        $user_ip = $_SERVER['REMOTE_ADDR'];
-        $current_page = basename($_SERVER['PHP_SELF']); 
-        
-        $stmt_track = $db->prepare("INSERT INTO analytics (user_ip, page) VALUES (?, ?)");
-        $stmt_track->execute([$user_ip, $current_page]);
-    } catch (PDOException $e) {
-    }
+try {
+    $user_ip = $_SERVER['REMOTE_ADDR'];
+    $current_page = basename($_SERVER['PHP_SELF']); 
+    
+    $stmt_track = $db->prepare("INSERT INTO analytics (user_ip, page) VALUES (?, ?)");
+    $stmt_track->execute([$user_ip, $current_page]);
+} catch (PDOException $e) {
+  
 }
 ?>
